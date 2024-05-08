53f1c57763cb4d9691809287b6108d228ff5386a
Tanmay Ambadkar <tanmay.ambadkar@gmail.com>
2024-05-08 14:20:22 -0400

    Added logging, changed environment

--- three_grid.py
+++ three_grid.py
@@ -2,6 +2,8 @@ from refinement.graph import Node, depth_first_traversal
 from refinement.goal import Goal
 from env.dirl_grid import RoomsEnv
 from env.rooms_envs import GRID_PARAMS_LIST, MAX_TIMESTEPS, START_ROOM, FINAL_ROOM
+import matplotlib.pyplot as plt
+from env.gridworldenv import ContinuousGridworld
 
 start_region = Goal(1, 1, 6, 6)
 mid_region = Goal(17, 1, 12, 6)
@@ -15,8 +17,21 @@ start_node.add_child(mid_node)
 mid_node.add_child(goal_node)
 
 
+custom_doors={
+    ((0, 0), (1, 0)): 6,
+    ((0, 0), (0, 1)): 6,
+    # ((1, 0), (1, 1)): 6,
+    ((1, 0), (2, 0)): 6,
+    ((2, 1), (2, 2)): 6,
+    ((0, 1), (1, 1)): 6,
+    ((1, 1), (2, 1)): 6,
+}
 
-env = RoomsEnv(GRID_PARAMS_LIST[0], START_ROOM[0], FINAL_ROOM[0], max_timesteps=MAX_TIMESTEPS[1])
+env = ContinuousGridworld(
+    custom_doors = custom_doors,
+    render=False
 
-def run_3grid(minimum_reach: float = 0.9, n_episodes: int = 3000):
-    depth_first_traversal(start_node, env, minimum_reach, n_episodes)
+)
+def run_3grid(minimum_reach: float = 0.9, n_episodes: int = 3000, n_episodes_test: int = 3000, path: str = ""):
+    depth_first_traversal(start_node, env, minimum_reach, n_episodes, n_episodes_test, path)
+  
